2a0edd74151c491e9dc634ac763e2bcf75d3f662
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-04-03 17:09:50 -0300

    Feito parte do enforcamento

--- src/JogoDaForca.ConsoleApp/Program.cs
+++ src/JogoDaForca.ConsoleApp/Program.cs
@@ -4,21 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("-------------------------------------");
-        Console.WriteLine("----------- Jogo da Forca -----------");
-        Console.WriteLine("-------------------------------------");
-        Console.WriteLine();
-
-        Console.WriteLine(" ___________");
-        Console.WriteLine(" |/        |");
-        Console.WriteLine(" |");
-        Console.WriteLine(" |");
-        Console.WriteLine(" |");
-        Console.WriteLine(" |");
-        Console.WriteLine(" |");
-        Console.WriteLine(" |");
-        Console.WriteLine("_|____");
-        Console.WriteLine();
+        MostrarMenu();
 
         string[] frutas = new string[29] { "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "BACABA", "BACURI", "BANANA", "CAJA", "CAJU", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇA", "MANGABA", "MANGA", "MARACUJA", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "BERGAMOTA", "UMBU", "UVA", "UVAIA" };
 
@@ -52,17 +38,51 @@ class Program
 
                 }
             }
-            
+
             string palavraEncontrada = string.Join("", palavraForca);
 
             if (palavraEncontrada == frutaAleatoria)
-                {
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Você acertou a palavra!");
+                Console.ForegroundColor = ConsoleColor.Gray;
                 break;
             }
+
+
             
+
             Console.WriteLine(palavraForca);
             Console.WriteLine();
         }
+        for (int i = 0; i  < frutaAleatoria.Length; i++)
+        {
+            if (palavraForca[i] == '_')
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Enforcado!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                break;
+            }
+        }
+    }
+
+    private static void MostrarMenu()
+    {
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("----------- Jogo da Forca -----------");
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine();
+
+        Console.WriteLine(" ___________");
+        Console.WriteLine(" |/        |");
+        Console.WriteLine(" |");
+        Console.WriteLine(" |");
+        Console.WriteLine(" |");
+        Console.WriteLine(" |");
+        Console.WriteLine(" |");
+        Console.WriteLine(" |");
+        Console.WriteLine("_|____");
+        Console.WriteLine();
     }
 }
